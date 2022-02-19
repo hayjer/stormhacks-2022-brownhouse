837907c5f7b85dd89c3200018c2665a91da0c8fe
Nand Patel <nand.pats@gmail.com>
2022-02-19 10:55:28 -0800

    homepage selection proccess implemented

---
+++ myscripts.js
@@ -0,0 +1,21 @@
+function setSelectedGenre(element) {
+    element = element.children[0];
+    if (element.classList.contains("selected-genre"))
+        element.classList.remove("selected-genre");
+    else {
+        if(document.getElementsByClassName("selected-genre").length >= 3)
+            return;
+        element.classList.add("selected-genre");
+    }
+}
+
+function setSelectedDate(element) {
+    element = element.children[0];
+    if (element.classList.contains("selected-time"))
+        element.classList.remove("selected-time");
+    else {
+        if(document.getElementsByClassName("selected-time").length >= 1)
+            return;
+        element.classList.add("selected-time");
+    }
+}
