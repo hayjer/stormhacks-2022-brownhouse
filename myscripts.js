88dc2749624e1b7279f3ade9eacf13b553f58129
Nand Patel <nand.pats@gmail.com>
2022-02-19 13:14:35 -0800

    Added modal on continue click

--- myscripts.js
+++ myscripts.js
@@ -21,7 +21,35 @@ function setSelectedDate(element) {
 }
 
 function sendRequest() {
-    document.getElementsByClassName
+    let genreElements = document.getElementsByClassName("selected-genre");
     let genres = [];
-    for(let i = 0; i < )
+    for(let i = 0; i < genreElements.length; i++) {
+        genres[i] = genreElements[i].innerHTML;
+    }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("continue");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
